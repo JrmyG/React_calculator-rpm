2588785e9286dec3a22fa744c2315992eec81e68
Akamis013 <alex-v-pg@hotmail.fr>
2019-11-22 14:17:48 +0100

    Update calculator

--- ecv_calculator/src/components/KeyPadComponent.js
+++ ecv_calculator/src/components/KeyPadComponent.js
@@ -6,9 +6,9 @@ class KeyPadComponent extends Component {
         return (
             <div className="button">
                 <button name="(" onClick={e => this.props.onClick(e.target.name)}>(</button>
-                <button name="CE" onClick={e => this.props.onClick(e.target.name)}>CE</button>
+                <button name="CE" onClick={e => this.props.onClick(e.target.name)}>Del</button>
                 <button name=")" onClick={e => this.props.onClick(e.target.name)}>)</button>
-                <button name="C" onClick={e => this.props.onClick(e.target.name)}>C</button><br/>
+                <button name="C" onClick={e => this.props.onClick(e.target.name)}>Del all </button><br/>
 
 
                 <button name="1" onClick={e => this.props.onClick(e.target.name)}>1</button>
